98b726024f4d0ec02ab8cc067c89168ff02d7d82
rzpy1997 <kingradomir@gmail.com>
2023-11-23 09:13:30 +0600

    'class'

--- main_page/urls.py
+++ main_page/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import main_view, film_detail_view
+from .views import MainView, FilmDetailView
 
 urlpatterns = [
-    path('main_page/', main_view),
-    path('film_detail/<int:id>/', film_detail_view),
+    path('', MainView.as_view()),
+    path('film_detail/<int:id>/', FilmDetailView),
 ]
--- main_page/views.py
+++ main_page/views.py
@@ -1,24 +1,36 @@
 from django.shortcuts import render, get_object_or_404
 from .models import StringRun, FilmModel, AfishaTable, Slider
+from django.views import generic
 
-def main_view(request):
-    if request.method == 'GET':
+
+class MainView(generic.View):
+    def get(self, request):
         string_ = StringRun.objects.all()
         film_list = FilmModel.objects.all()
         afisha_list = AfishaTable.objects.all()
         slider = Slider.objects.all()
-        return render(request, template_name='main_page/index.html',
-                      context={
-                          'string_': string_,
-                          'film_list': film_list,
-                          'afisha_list': afisha_list,
-                          'slider': slider,
-                      })
+        return render(
+            request,
+            template_name='main_page/index.html',
+            context={
+                'string_': string_,
+                'film_list': film_list,
+                'afisha_list': afisha_list,
+                'slider': slider,
+            }
+        )
 
 
-def film_detail_view(request, id):
-    if request.method == 'GET':
-        film_id = get_object_or_404(FilmModel, id=id)
-        return render(request, template_name='main_page/film_detail.html', context={
-            'film_id':film_id
-        })
+class FilmDetailView(generic.DetailView):
+    template_name = 'main_page/film_detail.html'
+
+    def get_object(self, **kwargs):
+        film_id = self.kwargs.get('id')
+        return get_object_or_404(FilmModel, id=film_id)
+
+# def film_detail_view(request, id):
+#     if request.method == 'GET':
+#         film_id = get_object_or_404(FilmModel, id=id)
+#         return render(request, template_name='main_page/film_detail.html', context={
+#             'film_id': film_id
+#         })
